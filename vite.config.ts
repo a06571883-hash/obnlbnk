460a04c4b4e55b39ff67888b947e0fbd3b01986e
dimaddd558 <40660573-dimaddd558@users.noreply.replit.com>
2025-03-19 19:42:26 +0000

    Restored to '02d8d5e471bea24ce2bb6b88ab4621cc7cd182e0'
    Replit-Restored-To: 02d8d5e471bea24ce2bb6b88ab4621cc7cd182e0

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
-import { cartographer } from "@replit/vite-plugin-cartographer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [
     react({
       fastRefresh: true
@@ -21,8 +18,13 @@ export default defineConfig(async () => ({
       }
     }),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
-      ? [cartographer()]
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+        ]
       : []),
   ],
   resolve: {
@@ -36,4 +38,4 @@ export default defineConfig(async () => ({
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
-}));
+});
